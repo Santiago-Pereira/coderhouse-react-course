674d83be24adb16c559e5800e1defd4e498855e5
SantiagoPereira <santiagopereira_8@hotmail.com>
2021-06-12 14:44:33 -0300

    first commit

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FaShoppingCart} from 'react-icons/fa'
+
+function CartWidget(greeting) {
+  return(
+  <FaShoppingCart/>
+  )
+}
+
+export default CartWidget;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+
+function ItemList() {
+  return(
+   <Container>
+     <Row>
+       <Col className="col">
+   <h1 className="text-center">aqui ira el catalogo de mis productos</h1>
+       </Col>
+    </Row>
+   </Container>
+  )
+}
+
+export default ItemList;
